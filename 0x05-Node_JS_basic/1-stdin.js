1be1e1dbf9cf667350d8d979628f1e8b1fc80dd9
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-11-27 03:17:16 +0200

    0x05 task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,11 @@
 function stdinFunction () {
-
+    console.log("Welcome to Holberton School, what is your name?");
+    process.stdin.on("data", (data) => {
+      const name = data.toString().trim();
+      console.log(`Your name is: ${name}`);
+      console.log("This important software is now closing");
+      process.exit();
+    });
 }
 
 stdinFunction();
