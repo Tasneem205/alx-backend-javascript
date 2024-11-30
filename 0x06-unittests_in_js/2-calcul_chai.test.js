00c5151528b5d0b8e7dff10b13d0b9a442040a14
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-11-30 03:48:24 +0200

    0x06 task 2.2

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,48 +1,47 @@
 let chai;
-let assert;
 const calculateNumber = require('./2-calcul_chai');
 
 (async () => {
   chai = await import('chai');
-  assert = chai.assert;
+  const expect = chai.expect;
 
   describe('calculateNumber', () => {
     describe('SUM', () => {
       it('should return 5 when inputs are 2.4 and 2.6', () => {
-        assert.strictEqual(calculateNumber('SUM', 2.4, 2.6), 5);
+        expect(calculateNumber('SUM', 2.4, 2.6)).to.equal(5);
       });
-  
+
       it('should return 8 when inputs are 4.5 and 3.6', () => {
-        assert.strictEqual(calculateNumber('SUM', 4.5, 3.6), 9);
+        expect(calculateNumber('SUM', 4.5, 3.6)).to.equal(9);
       });
     });
-  
+
     describe('SUBTRACT', () => {
       it('should return 0 when inputs are 3.5 and 3.5', () => {
-        assert.strictEqual(calculateNumber('SUBTRACT', 3.5, 3.5), 0);
+        expect(calculateNumber('SUBTRACT', 3.5, 3.5)).to.equal(0);
       });
-  
+
       it('should return 2 when inputs are 5.8 and 3.2', () => {
-        assert.strictEqual(calculateNumber('SUBTRACT', 5.8, 3.2), 3);
+        expect(calculateNumber('SUBTRACT', 5.8, 3.2)).to.equal(3);
       });
     });
-  
+
     describe('DIVIDE', () => {
       it('should return 1.75 when inputs are 3.5 and 2', () => {
-        assert.strictEqual(calculateNumber('DIVIDE', 3.5, 2), 2);
+        expect(calculateNumber('DIVIDE', 3.5, 2)).to.equal(2);
       });
-  
+
       it('should return Error when trying to divide by 0', () => {
-        assert.strictEqual(calculateNumber('DIVIDE', 3.5, 0), 'Error');
+        expect(calculateNumber('DIVIDE', 3.5, 0)).to.equal('Error');
       });
-  
+
       it('should return 2 when inputs are 4 and 2', () => {
-        assert.strictEqual(calculateNumber('DIVIDE', 4, 2), 2);
+        expect(calculateNumber('DIVIDE', 4, 2)).to.equal(2);
       });
-  
+
       it('should return 1.5 when inputs are 4.5 and 3', () => {
-        assert.strictEqual(calculateNumber('DIVIDE', 4.5, 3), 1.67);
+        expect(calculateNumber('DIVIDE', 4.5, 3)).to.be.closeTo(1.67, 0.01); // Using closeTo for floating point precision
       });
-    });  
+    });
   });
 })();
