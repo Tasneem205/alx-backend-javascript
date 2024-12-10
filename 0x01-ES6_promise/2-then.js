5cb49d3a1f79a208e81d11cec8e311d1f2b94a3f
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-12-10 07:36:46 +0200

    0x01 task 2.2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 function handleResponseFromAPI(promise) {
-  promise.then({ status: 200, body: 'success' })
-    .catch(Error())
+  promise.then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
     .finally(() => {
       console.log('Got a response from the API');
     });
