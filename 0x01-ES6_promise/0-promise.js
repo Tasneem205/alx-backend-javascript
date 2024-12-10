b31c399130e40f366afb59c2b574aeecf7e21ef2
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-12-10 06:41:25 +0200

    0x01 task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,8 @@
 function getResponseFromAPI() {
-  return new Promise(() => {
+  const myProm = new Promise(() => {
     console.log('No API found');
   });
+  return myProm;
 }
 
 exports.getResponseFromAPI = getResponseFromAPI;
