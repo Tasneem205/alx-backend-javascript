7a360f3a17e358f0eb62345a40d174f9863bca1a
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-12-11 22:16:40 +0200

    0x01 task 3.1

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from "./utils.js";
 
 function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
   .then(([photoResponse, userResponse]) => {
     // Log the required information
     console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
