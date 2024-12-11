f2b185f647bb83095afe10cd09c432ce3729ab13
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-12-11 22:12:44 +0200

    0x01 task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from "./utils.js";
+
+function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+  .then(([photoResponse, userResponse]) => {
+    // Log the required information
+    console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+  })
+  .catch(() => {
+    // Handle errors
+    console.log('Signup system offline');
+  });
+}
+
+export default handleProfileSignup;
