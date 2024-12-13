60cbf3229cb7b48331c9a45572becfe688e5714b
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-12-14 01:38:34 +0200

    some eslint edits but it didn't work

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,9 @@
 import { uploadPhoto, createUser } from "./utils.js";
 
-function handleProfileSignup() {
+export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
   .then(([photoResponse, userResponse]) => {
-    // Log the required information
     console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
   })
-  .catch(() => {
-    // Handle errors
-    console.log('Signup system offline');
-  });
+  .catch(() => { console.log('Signup system offline'); });
 }
-
-export default handleProfileSignup;
