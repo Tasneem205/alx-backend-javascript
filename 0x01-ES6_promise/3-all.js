5447bf803349991317c6b9784ae37d357c76230e
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-12-14 01:40:21 +0200

    some eslint edits

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,8 @@ import { uploadPhoto, createUser } from "./utils.js";
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-  .then(([photoResponse, userResponse]) => {
-    console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
-  })
-  .catch(() => { console.log('Signup system offline'); });
+    .then(([photoResponse, userResponse]) => {
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch(() => { console.log('Signup system offline'); });
 }
