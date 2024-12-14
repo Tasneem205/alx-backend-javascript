2cd3e5d0616e062d8dfb27626f4d99a43f40d9b1
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-12-14 18:03:40 +0200

    0x01 task 8.1

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,4 @@
 export default function divideFunction(numerator, denominator) {
-  const res = new Promise((resolve, reject) => {
-    if (denominator === 0) reject(Error('cannot divide by 0'));
-    else resolve(numerator / denominator);
-  });
-  return res;
+  if (denominator === 0) throw(Error('cannot divide by 0'));
+  else return numerator / denominator;
 }
