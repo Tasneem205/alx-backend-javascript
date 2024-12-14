53913ebbc192a9edd04c57c34cd5d99f0dea99d9
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-12-14 04:58:12 +0200

    0x01 task 6.2

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,12 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpPromise, uploadPromise])
+    .then((results) => [
+      {
+        status: results[0].status,
+        value: results[0].status === 'fulfilled' ? results[0].value : results[0].reason,
+      },
+      {
+        status: results[1].status,
+        value: results[1].status === 'fulfilled' ? results[1].value : results[1].reason,
+      },
+    ]);
 }
