cb3a49bf318c81feb7ca7a1cbe1a6de448fe587a
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-12-17 07:05:50 +0200

    0x03 task 8.1

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   return [...set]
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length))
+    .filter(value => typeof value === 'string' && value.indexOf(startString) === 0)
+    .map(value => value.slice(startString.length))
     .join('-');
 }
