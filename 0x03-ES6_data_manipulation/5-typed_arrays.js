ac279c478ce2fc327e95c9f2b610f88db2580b9a
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-12-17 06:37:17 +0200

    0x03 task 5

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,6 @@
+export default function createInt8TypedArray(length, position, value) {
+  const int8Array = new Int8Array(length);
+  if (position < 0 || position >= length) throw new Error("Position outside range");
+  int8Array[position] = value;
+  return int8Array;
+}
